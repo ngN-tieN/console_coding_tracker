e27741df38524247db5e48d01f76620644f0ec3d
tien <nq2017.nguyentien081202@gmail.com>
2024-03-12 21:25:34 +0700

    change all methods to static

--- codingTimeLogger/ValidationService.cs
+++ codingTimeLogger/ValidationService.cs
@@ -6,10 +6,9 @@ internal class ValidationService{
     
     
     
-    public bool ValDateTimeInput(string DateTimeInput){
+    public static bool ValDateTimeInput(string DateTimeInput){
         Regex DateRegex = new Regex("(\\d{2}-\\d{2}-\\d{2} \\d{2}:\\d{2})", RegexOptions.IgnoreCase);
         return DateRegex.IsMatch(DateTimeInput);
     }
 
-    
 }
