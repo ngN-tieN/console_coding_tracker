ff355e7a7937541307850fc005131aa8d4292996
tien <nq2017.nguyentien081202@gmail.com>
2024-03-12 21:25:07 +0700

    change all methods to static

--- codingTimeLogger/UserInput.cs
+++ codingTimeLogger/UserInput.cs
@@ -1,13 +1,13 @@
 using System;
 
 internal class UserInput{
-    public string GetDateTimeInput(string message){
+    public static string GetDateTimeInput(string message){
         Console.WriteLine(message);
         string dateInput = Console.ReadLine();
         return dateInput;
     }
 
-    public int GetNumberInput(string message){
+    public static int GetNumberInput(string message){
         Console.WriteLine(message);
         string number_string = Console.ReadLine();
         if(Int32.TryParse(number_string, out int number)) return number;
